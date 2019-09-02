4f0022be11baa47e7031f9eee51150279112ea09
luisca1985 <luisca1985@gmail.com>
2019-09-02 17:26:30 -0500

    Update Order.java

--- src/main/java/com/example/prueba/model/Order.java
+++ src/main/java/com/example/prueba/model/Order.java
@@ -6,13 +6,13 @@ import java.util.Date;
 import javax.persistence.*;
 import lombok.Data;
 
-@Data
-@Entity(name = "Order")
-@Table(name = "\"order\"")
+@Data // Crea todos los métodos getters, setters, equals, hash y toString, en función de los campos.
+@Entity(name = "Order") // Prepara este objeto para almacenarlo en un almacén de datos basado en JPA
+@Table(name = "\"order\"") // Se indica el nombre de la tabla de la base de datos a la que corresponde. Se utiliza \"order\" que representa el nombre "order" (entre comillas), ya que palabra order es una palabra reservada de mysql.
 public class Order{
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "\"order_id\"")
+	@Id // Indica que es la clave principal al proveedor JPA
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Indica que se llena automáticamente por el proveedro JPA
+	@Column(name = "\"order_id\"") // Indica la información de la columna en la base de datos
 	private int orderId;
 	@ManyToOne
 	@JoinColumn(name="\"customer_id\"")
